67cdf27b1d4fe64f7928c64c101b0f08859a92a6
Steve T <stevethewebguy@gmail.com>
2016-03-26 13:25:22 -0700

    dynamic url

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,10 +1,16 @@
 angular.module('OWMApp', ['ngRoute'])
-    .config(['$routeProvider', function($routeProvider){
+    .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {
             templateUrl : 'home.html',
             controller : 'HomeCtrl'
+        }).when('/cities/:city', {
+            templateUrl : 'city.html',
+            controller : 'CityCtrl'
         });
     }])
     .controller('HomeCtrl', function($scope) {
         //empty for now
-    });
+    })
+    .controller('CityCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+        $scope.city = $routeParams.city;
+    }]);
