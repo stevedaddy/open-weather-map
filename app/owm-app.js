a0cca4da4a3adb22a0f52f23e6ea24c25b420713
Steve T <stevethewebguy@gmail.com>
2016-03-26 14:46:22 -0700

    animated routes

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,16 +1,49 @@
-angular.module('OWMApp', ['ngRoute'])
-    .config(['$routeProvider', function($routeProvider) {
+angular.module('owmApp', ['ngRoute', 'ngAnimate'])
+    .value('ownCities',['New York', 'Dallas', 'Chicago'])
+
+    .controller('HomeCtrl', function($scope) {
+        //empty for now
+    })
+
+    .config(function($routeProvider) {
         $routeProvider.when('/', {
             templateUrl : 'home.html',
             controller : 'HomeCtrl'
         }).when('/cities/:city', {
             templateUrl : 'city.html',
-            controller : 'CityCtrl'
-        });
-    }])
-    .controller('HomeCtrl', function($scope) {
-        //empty for now
+            controller : 'CityCtrl',
+            resolve: {
+                city : function(ownCities, $route, $location) {
+                    var city = $route.current.params.city;
+                    if(ownCities.indexOf(city) == -1) {
+                        throw new Error("City not found");
+                    }
+                    return city;
+                }
+            }
+        })
+            .when('/error', {
+                template : 'Error Page Not Found'
+            })
+            .otherwise({
+                redirectTo : '/error'
+            });
     })
-    .controller('CityCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
-        $scope.city = $routeParams.city;
-    }]);
+
+    .controller('CityCtrl', function($scope, city) {
+        $scope.city = city;
+    })
+
+    .run(function($rootScope, $location, $timeout) {
+        $rootScope.$on('$routeChangeError', function() {
+            $location.path("/error");
+        });
+        $rootScope.$on('$routeChangeStart', function() {
+            $rootScope.isLoading = true;
+        });
+        $rootScope.$on('$routeChangeSuccess', function() {
+            $timeout(function() {
+                $rootScope.isLoading = false;
+            }, 1000);
+        });
+    });
