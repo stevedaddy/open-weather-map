95bf2a1a1520f722379708fde4b10084473a02fb
Steve Tchorzewski <stchorzewski@atypicalbrands.com>
2016-03-28 16:44:54 -0700

    Update to use gif instead of svg

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,32 +1,49 @@
-angular.module('OWMApp', ['ngRoute'])
-    .value('owmCities',
-        ['New York', 'Dallas', 'Chicago'])
-	.config(function($routeProvider){
-        $routeProvider.when('/', {
-            templateUrl: './home.html',
-            controller : 'HomeCtrl'
-        })
-        .when('/cities/:city', {
-            templateUrl : 'city.html',
-            controller : 'CityCtrl',
-            resolve : {
-                city: function(owmCities, $route, $location) {
-                    var city = $route.current.params.city;
-                    if(owmCities.indexOf(city) == -1 ) {
-                        $location.path('/error');
-                        return;
-                    }
-                    return city;
-                }
-            }
-        })
-        .when('/error', {
-		      template : '<p>Error Page Not Found</p>'
-		});
-    })
+angular.module('owmApp', ['ngRoute', 'ngAnimate'])
+	.value('ownCities',['New York', 'Dallas', 'Chicago'])
+
     .controller('HomeCtrl', function($scope) {
-        //empty for now
-    })
-    .controller('CityCtrl', function($scope, city) {
-   		$scope.city = city;
+			//empty for now
+		})
+
+  .config(function($routeProvider) {
+      $routeProvider.when('/', {
+        templateUrl : 'home.html',
+        controller : 'HomeCtrl'
+      }).when('/cities/:city', {
+        templateUrl : 'city.html',
+        controller : 'CityCtrl',
+        resolve: {
+          city : function(ownCities, $route, $location) {
+				    var city = $route.current.params.city;
+				    if(ownCities.indexOf(city) == -1) {
+					    throw new Error("City not found");
+			    	}
+				    return city;
+			    }
+        }
+      })
+	    .when('/error', {
+		    template : 'Error Page Not Found'
+	    })
+  		.otherwise({
+  			redirectTo : '/error'
+  		});
+		})
+
+	.controller('CityCtrl', function($scope, city) {
+		$scope.city = city;
+	})
+
+	.run(function($rootScope, $location, $timeout) {
+		$rootScope.$on('$routeChangeError', function() {
+			$location.path("/error");
+		});
+		$rootScope.$on('$routeChangeStart', function() {
+			$rootScope.isLoading = true;
+		});
+		$rootScope.$on('$routeChangeSuccess', function() {
+		  $timeout(function() {
+  			$rootScope.isLoading = false;
+		  }, 1000);
+		});
 	});
